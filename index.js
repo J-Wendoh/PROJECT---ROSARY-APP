61f5387725de173bddba7bb2390ef3e644cf0fa9
Your Name <your-email@example.com>
2024-04-10 23:16:56 +0300

    add changes

--- index.js
+++ index.js
@@ -5,31 +5,171 @@ const wbtn=document.getElementById("wednesday")
 const thbtn=document.getElementById("thursday")
 const fbtn=document.getElementById("friday")
 const satbtn=document.getElementById("saturday")
+const position=document.getElementById("location")
+const todbtn=document.getElementById("today")
+
 
 
 
 addEventListener("DOMContentLoaded", (event) => {
 
+  //SUNDAY
+  sbtn.addEventListener("click",grabsun)
 
-function getsundata(){
-   fetch(`https://the-rosary-api.vercel.app/v1/sunday`)
-            .then( function (response) {
-                return response.json();
-            })
-            .then(function (data){
-              console.log(data)
-                let par=document.createElement("p")
-                document.querySelector("#location").prepend(par)
-                par.textContent=`${object}`
-                const object=function objectthrough(){
-                  for(key of data){return key.data}
-            }
-          }
-            )}
-sbtn.addEventListener("click",getsundata)
+  function grabsun(){
+  fetch('https://the-rosary-api.vercel.app/v1/sunday')
+  .then(response => response.json()) // Parse the response as JSON
+  .then(data => {
+  console.log(data)
+    const jsonData = [data][0]
+    const par = document.createElement('p');
+    par.textContent = `Let us Start today's prayer, ${jsonData}`;
+    position.prepend(par);
+  })
+  .catch(error => {
+    // Handle any errors that occur during the fetch operation
+    console.error('Error fetching data:', error);
+  });
+  }
 
 
-            
+//MONDAY
+  mbtn.addEventListener("click",grabmon)
+
+  function grabmon(){
+  fetch('https://the-rosary-api.vercel.app/v1/monday')
+  .then(response => response.json()) // Parse the response as JSON
+  .then(data => {
+  console.log(data)
+    const jsonData = [data][0]
+    const par = document.createElement('p');
+    par.textContent = `Let us Start today's prayer, ${jsonData}`;
+    position.prepend(par);
+  })
+  .catch(error => {
+    // Handle any errors that occur during the fetch operation
+    console.error('Error fetching data:', error);
+  });
+  }
+
+  //TUESDAY
+  tbtn.addEventListener("click",grabtue)
+
+  function grabtue(){
+  fetch('https://the-rosary-api.vercel.app/v1/tuesday')
+  .then(response => response.json()) // Parse the response as JSON
+  .then(data => {
+  console.log(data)
+    const jsonData = [data][0]
+    const par = document.createElement('p');
+    par.textContent = `Let us Start today's prayer, ${jsonData}`;
+    position.prepend(par);
+  })
+  .catch(error => {
+    // Handle any errors that occur during the fetch operation
+    console.error('Error fetching data:', error);
+  });
+  }
+
+  //WEDNESDAY
+  wbtn.addEventListener("click",grabwed)
+
+  function grabwed(){
+  fetch('https://the-rosary-api.vercel.app/v1/wednesday')
+  .then(response => response.json()) // Parse the response as JSON
+  .then(data => {
+  console.log(data)
+    const jsonData = [data][0]
+    const par = document.createElement('p');
+    par.textContent = `Let us Start today's prayer, ${jsonData}`;
+    position.prepend(par);
+  })
+  .catch(error => {
+    // Handle any errors that occur during the fetch operation
+    console.error('Error fetching data:', error);
+  });
+  }
+
+  //THURSDAY
+  thbtn.addEventListener("click",grabthur)
+
+  function grabthur(){
+  fetch('https://the-rosary-api.vercel.app/v1/thursday')
+  .then(response => response.json()) // Parse the response as JSON
+  .then(data => {
+  console.log(data)
+    const jsonData = [data][0]
+    const par = document.createElement('p');
+    par.textContent = `Let us Start today's prayer, ${jsonData}`;
+    position.prepend(par);
+  })
+  .catch(error => {
+    // Handle any errors that occur during the fetch operation
+    console.error('Error fetching data:', error);
+  });
+  }
+
+  //FRIDAY
+  fbtn.addEventListener("click",grabfri)
+
+  function grabfri(){
+  fetch('https://the-rosary-api.vercel.app/v1/friday')
+  .then(response => response.json()) // Parse the response as JSON
+  .then(data => {
+  console.log(data)
+    const jsonData = [data][0]
+    const par = document.createElement('p');
+    par.textContent = `Let us Start today's prayer, ${jsonData}`;
+    position.prepend(par);
+  })
+  .catch(error => {
+    // Handle any errors that occur during the fetch operation
+    console.error('Error fetching data:', error);
+  });
+  }
+
+
+  //SATURDAY
+  satbtn.addEventListener("click",grabsat)
+
+  function grabsat(){
+  fetch('https://the-rosary-api.vercel.app/v1/saturday')
+  .then(response => response.json()) // Parse the response as JSON
+  .then(data => {
+  console.log(data)
+    const jsonData = [data][0]
+    const par = document.createElement('p');
+    par.textContent = `Let us Start today's prayer, ${jsonData}`;
+    position.prepend(par);
+  })
+  .catch(error => {
+    // Handle any errors that occur during the fetch operation
+    console.error('Error fetching data:', error);
+  });
+  }
+
+
+  //TODAY
+  todbtn.addEventListener("click",grabtod)
+
+  function grabtod(){
+  fetch('https://the-rosary-api.vercel.app/v1/today')
+  .then(response => response.json()) // Parse the response as JSON
+  .then(data => {
+  console.log(data)
+    const jsonData = [data][0]
+    const par = document.createElement('p');
+    par.textContent = `Let us Start today's prayer, ${jsonData}`;
+    position.prepend(par);
+  })
+  .catch(error => {
+    // Handle any errors that occur during the fetch operation
+    console.error('Error fetching data:', error);
+  });
+  }
+
+
+ 
           })
 
 
